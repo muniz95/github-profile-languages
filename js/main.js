71a1d480d8782df0160d1d41e6bde5bfc460b1dc
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-05-14 13:15:09 -0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,126 @@
-"use strict";(function(a){var b=a.querySelector.bind(a),c=function(b){return[].slice.call(a.querySelectorAll(b))};Element.prototype.closest||(Element.prototype.closest=function(a){for(var b;null!==(void 0).parentNode;){var c=b.parentNode;if(null===c)return null;if(c.querySelector(a)===b)return b;b=(void 0).parentNode}});var d=function(a){for(;a.firstChild;)a.removeChild(a.firstChild)};window.addEventListener("load",function(){function a(){var b=Url.queryString("user"),c=decodeURIComponent(b);if(!c)return void(g.textContent="GitHub Profile");d(g);var l=document.createElement("a");l.setAttribute("target","blank"),l.href="https://github.com/"+("@"===c[0]?c.slice(1):c),l.textContent="@"===c[0]?c:"@"+c,g.appendChild(l),g.appendChild(document.createTextNode("'s")),e.src="api.html?"+c,f.value=c,h.value="<iframe width=\"600\" height=\"600\" src=\"https://ionicabizau.github.io/github-profile-languages/api.html?"+c+"\" frameborder=\"0\"></iframe>";var a=encodeURI(location.href);j.href="https://www.facebook.com/sharer/sharer.php?u="+a,i.href="http://twitter.com/intent/tweet?text=A pie graph with my programming languages.&url="+a,k.href="https://plus.google.com/share?url="+a}var e=b("#graph"),f=b(".form-elm"),g=b(".username"),h=b(".embed textarea"),i=b(".popup a.twitter"),j=b(".popup a.facebook"),k=b(".popup a.gplus"),l=null;f.addEventListener("input",function(){Url.updateSearchParam("user",this.value),clearTimeout(l),l=setTimeout(function(){a()},1e3)}),a(),document.addEventListener("click",function(a){var c=a.target;c.dataset.popup!==void 0&&b(c.dataset.popup).classList.toggle("open")}),document.addEventListener("click",function(a){var b=a.target,c=void 0;b.classList.contains("close")&&null!==(c=b.closest(".popup"))&&c.classList.toggle("open")}),window.addEventListener("keydown",function(a){27===a.which&&c(".popup.open").forEach(function(a){a.classList.remove(".open")})})})})(document);
+"use strict";
+
+(function (doc) {
+
+    var qs = doc.querySelector.bind(doc);
+    var qsa = function qsa(selector) {
+        return [].slice.call(doc.querySelectorAll(selector));
+    };
+
+    if (!Element.prototype.closest) {
+        /**
+         * Shim for Element.closest
+         *
+         * Goes up in the DOM tree for the context element and
+         * finds elements matching a passed selector.
+         *
+         * @param {string} selector
+         * @returns {Element|null}
+         */
+        Element.prototype.closest = function (selector) {
+            var currentElement = undefined;
+            while (undefined.parentNode !== null) {
+                var currentParent = currentElement.parentNode;
+                if (currentParent === null) {
+                    return null;
+                }
+                if (currentParent.querySelector(selector) === currentElement) {
+                    return currentElement;
+                }
+
+                currentElement = undefined.parentNode;
+            }
+        };
+    }
+
+    var clearChildren = function clearChildren(element) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    };
+
+    window.addEventListener("load", function () {
+
+        //Elements
+        var iframe = qs("#graph"),
+            input = qs(".form-elm"),
+            username = qs(".username"),
+            embed = qs(".embed textarea"),
+            twitter = qs(".popup a.twitter"),
+            facebook = qs(".popup a.facebook"),
+            gplus = qs(".popup a.gplus"),
+            apiUrl = "https://ionicabizau.github.io/github-profile-languages/api.html";
+
+        /**
+         * check
+         * Updates the user info
+         *
+         * @name check
+         * @function
+         * @return {undefined}
+         */
+        function check() {
+            var queryStringUser = Url.queryString("user");
+            var user = decodeURIComponent(queryStringUser);
+            if (!user) {
+                username.textContent = "GitHub Profile";
+                return;
+            }
+            clearChildren(username);
+            var a = document.createElement("a");
+            a.setAttribute("target", "blank");
+            a.href = "https://github.com/" + (user[0] === "@" ? user.slice(1) : user);
+            a.textContent = user[0] === "@" ? user : "@" + user;
+
+            username.appendChild(a);
+            username.appendChild(document.createTextNode("'s"));
+
+            iframe.src = "api.html?" + user;
+            input.value = user;
+            embed.value = "<iframe width=\"600\" height=\"600\" src=\"" + apiUrl + "?" + user + "\" frameborder=\"0\"></iframe>";
+
+            // Update social
+            var escaped = encodeURI(location.href);
+
+            facebook.href = "https://www.facebook.com/sharer/sharer.php?u=" + escaped;
+            twitter.href = "http://twitter.com/intent/tweet?text=A pie graph with my programming languages.&url=" + escaped;
+            gplus.href = "https://plus.google.com/share?url=" + escaped;
+        }
+
+        // Writing the username
+        var timer = null;
+        input.addEventListener("input", function () {
+            Url.updateSearchParam("user", this.value);
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                check();
+            }, 1000);
+        });
+
+        check();
+
+        // Popups
+        document.addEventListener("click", function (event) {
+            var target = event.target;
+            if (target.dataset.popup !== undefined) {
+                qs(target.dataset.popup).classList.toggle("open");
+            }
+        });
+
+        document.addEventListener("click", function (event) {
+            var target = event.target;
+            var parentPopup = void 0;
+            if (target.classList.contains("close") && (parentPopup = target.closest(".popup")) !== null) {
+                parentPopup.classList.toggle("open");
+            }
+        });
+
+        window.addEventListener("keydown", function (e) {
+            if (e.which === 27) {
+                qsa(".popup.open").forEach(function (el) {
+                    el.classList.remove(".open");
+                });
+            }
+        });
+    });
+})(document);
--- js/pie.js
+++ js/pie.js
@@ -1,1 +1,300 @@
-'use strict';(function(){function a(){for(var a=1;a<arguments.length;a++)for(var b in arguments[a])arguments[a].hasOwnProperty(b)&&(arguments[0][b]=arguments[a][b]);return arguments[0]}function b(a){a.animation?requestAnimFrame(c):drawPieSegments(1)}function c(){animCount+=animFrameAmount,drawPieSegments(easingFunction(animCount)),1>animCount?requestAnimFrame(arguments.callee):settings.afterDrawed.call(this)}function d(a){return Math.min.apply(null,a)}var e=function(a){var b=document.createElement('div');return b.innerHTML=a,b.childNodes[0]};window.drawPieChart=function(c,f){function h(a){var b=this.dataset.order;J.textContent=c[b].title+': '+c[b].value,J.style.opacity=1,'active'!==A[b].getAttribute('data-active')&&(C[b].style.opacity=.8),w.onPieMouseenter.apply(this,[a,c])}function j(a){var b=this.dataset.order;J.style.opacity=0,'active'!==A[b].getAttribute('data-active')&&(C[b].style.opacity=w.lightPiesOpacity),w.onPieMouseleave.apply(this,[a,c])}function k(a){J.style.top=a.pageY+w.tipOffsetY+'px',J.style.left=a.pageX-J.clientWidth/2+w.tipOffsetX+'px'}function l(a){for(var b=this.dataset.order,d=A[b],e=0,f=c.length;e<f;e++)e!==b&&(A[e].setAttribute('data-active',''),C[e].style.opacity=w.lightPiesOpacity);'active'===d.getAttribute('data-active')?(d.setAttribute('data-active',''),C[b].style.opacity=.8):(d.setAttribute('data-active','active'),C[b].style.opacity=1),w.onPieClick.apply(this,[a,c])}var m,n=this.clientWidth,o=this.clientHeight,q=0;f.legend&&(m=document.createElementNS('http://www.w3.org/2000/svg','g'),m.classList.add('legend'),c.forEach(function(a){var b=document.createElementNS('http://www.w3.org/2000/svg','g'),c=document.createElementNS('http://www.w3.org/2000/svg','rect');c.setAttribute('width','10'),c.setAttribute('height','10'),b.setAttribute('transform','translate(0,'+(150+(q+=20))+')');var d=document.createElementNS('http://www.w3.org/2000/svg','text');d.innerHTML=a.title,d.setAttribute('y',11),d.setAttribute('x',20),c.setAttribute('fill',a.color),b.appendChild(c),b.appendChild(d),m.appendChild(b)}),o-=q);var r=n/2,s=o/2,t=Math.cos,u=Math.sin,v=Math.PI,w=a({segmentShowStroke:!0,segmentStrokeColor:'#fff',segmentStrokeWidth:1,baseColor:'#fff',baseOffset:15,edgeOffset:30,pieSegmentGroupClass:'pieSegmentGroup',pieSegmentClass:'pieSegment',lightPiesOffset:12,lightPiesOpacity:.3,lightPieClass:'lightPie',animation:!0,animationSteps:90,animationEasing:'easeInOutExpo',tipOffsetX:0,tipOffsetY:-50,tipClass:'pieTip',beforeDraw:function a(){},afterDrawed:function a(){},onPieMouseenter:function a(){},onPieMouseleave:function a(){},onPieClick:function a(){},legend:!1},f),x=function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(a){window.setTimeout(a,1e3/60)}}(),z=e('<svg width="'+n+'" height="'+(o+q)+'" viewBox="0 0 '+n+' '+(o+q)+'" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg>');this.appendChild(z);var A=[],B=[],C=[],D={linear:function b(a){return a},easeInOutExpo:function c(a){var b=.5>a?8*a*a*a*a:1-8*--a*a*a*a;return 1<b?1:b}}[w.animationEasing],E=d([o/2,n/2])-w.edgeOffset,F=0,G=function(){var a=document.createElementNS('http://www.w3.org/2000/svg','circle');z.appendChild(a),a.classList.add('pie'),a.setAttribute('cx',r),a.setAttribute('cy',s),a.setAttribute('r',E+w.baseOffset),a.setAttribute('fill',w.baseColor)}();f.legend&&m.setAttribute('transform','translate(0,'+E+')');var I=document.createElementNS('http://www.w3.org/2000/svg','g');I.setAttribute('opacity',0),z.appendChild(I);var J=document.createElement('div');w.tipClass&&J.classList.add(w.tipClass),J.style.transition='opacity 0.2s',document.body.appendChild(J),J.style.opacity=0;for(var K=J.clientWidth,L=J.clientHeight,M=0,N=c.length;M<N;M++){F+=c[M].value;var Q=document.createElementNS('http://www.w3.org/2000/svg','g');'undefined'==typeof Q.dataset&&(Q.dataset={}),Q.dataset.order=M,Q.setAttribute('class',w.pieSegmentGroupClass),A[M]=Q,I.appendChild(Q),A[M].addEventListener('mouseenter',h),A[M].addEventListener('mouseleave',j),A[M].addEventListener('mousemove',k),A[M].addEventListener('click',l);var g=document.createElementNS('http://www.w3.org/2000/svg','path');g.setAttribute('stroke-width',w.segmentStrokeWidth),g.setAttribute('stroke',w.segmentStrokeColor),g.setAttribute('stroke-miterlimit',2),g.setAttribute('fill',c[M].color),g.setAttribute('class',w.pieSegmentClass),B[M]=g,A[M].appendChild(g),f.legend&&z.appendChild(m);var p=document.createElementNS('http://www.w3.org/2000/svg','path');p.setAttribute('stroke-width',w.segmentStrokeWidth),p.setAttribute('stroke',w.segmentStrokeColor),p.setAttribute('stroke-miterlimit',2),p.setAttribute('fill',c[M].color),p.setAttribute('opacity',w.lightPiesOpacity),p.setAttribute('class',w.lightPieClass),p.style.transition='opacity .1s',C[M]=p,A[M].appendChild(p)}w.beforeDraw.call(this),b(w);var O=w.animation?1/w.animationSteps:1,P=w.animation?0:1;return this}})();
+'use strict';
+
+/*!
+ * jquery.drawPieChart.js
+ * Version: 0.3(Beta)
+ * Inspired by Chart.js(http://www.chartjs.org/)
+ *
+ * Copyright 2013 hiro
+ * https://github.com/githiro/drawPieChart
+ * Released under the MIT license.
+ *
+ * Modified by Madara Uchiha for non-jQuery environment.
+ */
+;(function () {
+  function extend() {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        if (arguments[i].hasOwnProperty(key)) {
+          arguments[0][key] = arguments[i][key];
+        }
+      }
+    }
+    return arguments[0];
+  }
+
+  var firstElementFromHtmlString = function firstElementFromHtmlString(string) {
+    var div = document.createElement('div');
+    div.innerHTML = string;
+    return div.childNodes[0];
+  };
+
+  function triggerAnimation(settings) {
+    if (settings.animation) {
+      requestAnimFrame(animationLoop);
+    } else {
+      drawPieSegments(1);
+    }
+  }
+
+  function animationLoop() {
+    animCount += animFrameAmount; //animCount start from 0, after "settings.animationSteps"-times executed, animCount reaches 1.
+    drawPieSegments(easingFunction(animCount));
+    if (animCount < 1) {
+      requestAnimFrame(arguments.callee);
+    } else {
+      settings.afterDrawed.call(this);
+    }
+  }
+
+  function Max(arr) {
+    return Math.max.apply(null, arr);
+  }
+
+  function Min(arr) {
+    return Math.min.apply(null, arr);
+  }
+
+  window.drawPieChart = function (data, options) {
+    var W = this.clientWidth,
+        H = this.clientHeight,
+        y = 0,
+        legend = void 0;
+
+    if (options.legend) {
+      legend = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+      legend.classList.add("legend");
+      data.forEach(function (cData) {
+        var c = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        rect.setAttribute("width", "10");
+        rect.setAttribute("height", "10");
+        c.setAttribute("transform", "translate(0," + (150 + (y += 20)) + ")");
+        var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        text.innerHTML = cData.title;
+        text.setAttribute("y", 11);
+        text.setAttribute("x", 20);
+
+        rect.setAttribute("fill", cData.color);
+
+        c.appendChild(rect);
+        c.appendChild(text);
+        legend.appendChild(c);
+      });
+      H -= y;
+    }
+
+    var centerX = W / 2,
+        centerY = H / 2,
+        cos = Math.cos,
+        sin = Math.sin,
+        PI = Math.PI,
+        settings = extend({
+      segmentShowStroke: true,
+      segmentStrokeColor: "#fff",
+      segmentStrokeWidth: 1,
+      baseColor: "#fff",
+      baseOffset: 15,
+      edgeOffset: 30, //offset from edge of this
+      pieSegmentGroupClass: "pieSegmentGroup",
+      pieSegmentClass: "pieSegment",
+      lightPiesOffset: 12, //lighten pie's width
+      lightPiesOpacity: .3, //lighten pie's default opacity
+      lightPieClass: "lightPie",
+      animation: true,
+      animationSteps: 90,
+      animationEasing: "easeInOutExpo",
+      tipOffsetX: 0,
+      tipOffsetY: -50,
+      tipClass: "pieTip",
+      beforeDraw: function beforeDraw() {},
+      afterDrawed: function afterDrawed() {},
+      onPieMouseenter: function onPieMouseenter(e, data) {},
+      onPieMouseleave: function onPieMouseleave(e, data) {},
+      onPieClick: function onPieClick(e, data) {},
+      legend: false
+    }, options),
+        animationOptions = {
+      linear: function linear(t) {
+        return t;
+      },
+      easeInOutExpo: function easeInOutExpo(t) {
+        var v = t < .5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
+        return v > 1 ? 1 : v;
+      }
+    },
+        requestAnimFrame = function () {
+      return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+    }();
+
+    var wrapper = firstElementFromHtmlString('<svg width="' + W + '" height="' + (H + y) + '" viewBox="0 0 ' + W + ' ' + (H + y) + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg>');
+    this.appendChild(wrapper);
+
+    var groups = [],
+        pies = [],
+        lightpies = [],
+        easingFunction = animationOptions[settings.animationEasing],
+        pieRadius = Min([H / 2, W / 2]) - settings.edgeOffset,
+        segmentTotal = 0;
+
+    //Draw base circle
+    var drawBasePie = function () {
+      var base = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      wrapper.appendChild(base);
+      base.classList.add("pie");
+      base.setAttribute("cx", centerX);
+      base.setAttribute("cy", centerY);
+      base.setAttribute("r", pieRadius + settings.baseOffset);
+      base.setAttribute("fill", settings.baseColor);
+    }();
+
+    if (options.legend) {
+      legend.setAttribute("transform", "translate(0," + pieRadius + ")");
+    }
+
+    //Set up pie segments wrapper
+    var pathGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    pathGroup.setAttribute("opacity", 0);
+    wrapper.appendChild(pathGroup);
+
+    //Set up tooltip
+    var tip = document.createElement('div');
+    if (settings.tipClass) {
+      tip.classList.add(settings.tipClass);
+    }
+    tip.style.transition = 'opacity 0.2s';
+    document.body.appendChild(tip);
+    tip.style.opacity = 0;
+    var tipW = tip.clientWidth,
+        tipH = tip.clientHeight;
+
+    for (var i = 0, len = data.length; i < len; i++) {
+      segmentTotal += data[i].value;
+      var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+      // Firefox only has the .dataset property on HTMLElements. This is an Element.
+      // Adding minimal shim.
+      if (typeof g.dataset === 'undefined') {
+        g.dataset = {};
+      }
+      g.dataset.order = i;
+      g.setAttribute("class", settings.pieSegmentGroupClass);
+      groups[i] = g;
+      pathGroup.appendChild(g);
+      groups[i].addEventListener("mouseenter", pathMouseEnter);
+      groups[i].addEventListener("mouseleave", pathMouseLeave);
+      groups[i].addEventListener("mousemove", pathMouseMove);
+      groups[i].addEventListener("click", pathClick);
+
+      var p = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      p.setAttribute("stroke-width", settings.segmentStrokeWidth);
+      p.setAttribute("stroke", settings.segmentStrokeColor);
+      p.setAttribute("stroke-miterlimit", 2);
+      p.setAttribute("fill", data[i].color);
+      p.setAttribute("class", settings.pieSegmentClass);
+      pies[i] = p;
+      groups[i].appendChild(p);
+
+      if (options.legend) {
+        wrapper.appendChild(legend);
+      }
+
+      var lp = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      lp.setAttribute("stroke-width", settings.segmentStrokeWidth);
+      lp.setAttribute("stroke", settings.segmentStrokeColor);
+      lp.setAttribute("stroke-miterlimit", 2);
+      lp.setAttribute("fill", data[i].color);
+      lp.setAttribute("opacity", settings.lightPiesOpacity);
+      lp.setAttribute("class", settings.lightPieClass);
+
+      lp.style.transition = 'opacity .1s';
+      lightpies[i] = lp;
+      groups[i].appendChild(lp);
+    }
+
+    settings.beforeDraw.call(this);
+    //Animation start
+    triggerAnimation(settings);
+
+    function pathMouseEnter(e) {
+      var index = this.dataset.order;
+      tip.textContent = data[index].title + ": " + data[index].value;
+      tip.style.opacity = 1;
+      if (groups[index].getAttribute("data-active") !== "active") {
+        lightpies[index].style.opacity = .8;
+      }
+      settings.onPieMouseenter.apply(this, [e, data]);
+    }
+    function pathMouseLeave(e) {
+      var index = this.dataset.order;
+      tip.style.opacity = 0;
+      if (groups[index].getAttribute("data-active") !== "active") {
+        lightpies[index].style.opacity = settings.lightPiesOpacity;
+      }
+      settings.onPieMouseleave.apply(this, [e, data]);
+    }
+    function pathMouseMove(e) {
+      tip.style.top = e.pageY + settings.tipOffsetY + "px";
+      tip.style.left = e.pageX - tip.clientWidth / 2 + settings.tipOffsetX + "px";
+    }
+    function pathClick(e) {
+      var index = this.dataset.order;
+      var targetGroup = groups[index];
+      for (var _i = 0, _len = data.length; _i < _len; _i++) {
+        if (_i === index) continue;
+        groups[_i].setAttribute("data-active", "");
+        lightpies[_i].style.opacity = settings.lightPiesOpacity;
+      }
+      if (targetGroup.getAttribute("data-active") === "active") {
+        targetGroup.setAttribute("data-active", "");
+        lightpies[index].style.opacity = .8;
+      } else {
+        targetGroup.setAttribute("data-active", "active");
+        lightpies[index].style.opacity = 1;
+      }
+      settings.onPieClick.apply(this, [e, data]);
+    }
+
+    var drawPieSegments = function drawPieSegments(animationDecimal) {
+      var startRadius = -PI / 2; //-90 degree
+      var rotateAnimation = 1;
+
+      if (settings.animation) {
+        rotateAnimation = animationDecimal; //count up between0~1
+      }
+
+      pathGroup.setAttribute("opacity", animationDecimal);
+
+      //draw each path
+      for (var _i2 = 0, _len2 = data.length; _i2 < _len2; _i2++) {
+        var segmentAngle = rotateAnimation * (data[_i2].value / segmentTotal * (PI * 2)),
+            //start radian
+        endRadius = startRadius + segmentAngle,
+            largeArc = (endRadius - startRadius) % (PI * 2) > PI ? 1 : 0,
+            startX = centerX + cos(startRadius) * pieRadius,
+            startY = centerY + sin(startRadius) * pieRadius,
+            endX = centerX + cos(endRadius) * pieRadius,
+            endY = centerY + sin(endRadius) * pieRadius,
+            startX2 = centerX + cos(startRadius) * (pieRadius + settings.lightPiesOffset),
+            startY2 = centerY + sin(startRadius) * (pieRadius + settings.lightPiesOffset),
+            endX2 = centerX + cos(endRadius) * (pieRadius + settings.lightPiesOffset),
+            endY2 = centerY + sin(endRadius) * (pieRadius + settings.lightPiesOffset);
+        var cmd = ['M', startX, startY, //Move pointer
+        'A', pieRadius, pieRadius, 0, largeArc, 1, endX, endY, //Draw outer arc path
+        'L', centerX, centerY, //Draw line to the center.
+        'Z' //Cloth path
+        ];
+        var cmd2 = ['M', startX2, startY2, 'A', pieRadius + settings.lightPiesOffset, pieRadius + settings.lightPiesOffset, 0, largeArc, 1, endX2, endY2, //Draw outer arc path
+        'L', centerX, centerY, 'Z'];
+        pies[_i2].setAttribute("d", cmd.join(' '));
+        lightpies[_i2].setAttribute("d", cmd2.join(' '));
+        startRadius += segmentAngle;
+      }
+    };
+
+    var animFrameAmount = settings.animation ? 1 / settings.animationSteps : 1; //if settings.animationSteps is 10, animFrameAmount is 0.1
+    var animCount = settings.animation ? 0 : 1;
+    return this;
+  };
+})();
